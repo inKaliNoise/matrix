c3c80696c31001cffa1ffe608e2d19e2f914d048
Konrad <kooonrad.noise@gmail.com>
2020-07-24 21:57:10 +0000

    add init projects

---
+++ app.py
@@ -0,0 +1,16 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+matrix.append([10, 11, 12])
+print(matrix)
+for row in matrix:
+    for item in row:
+        print(item)
+
+
+numbers = [1, 2, 3,]
+numbers.clear()
+numbers.append(4)
+print(numbers)
---
+++ dictonaries.py
@@ -0,0 +1,19 @@
+
+digit_mapping = {
+    '1': ' one',
+    '2': ' two',
+    '3': ' three',
+    '4': ' four',
+    '5': ' five',
+    '6': ' six',
+    '7': ' seven',
+    '8': ' eight',
+    '9': ' nine'
+}
+answer = ''
+choose = []
+choose = input('phone: ')
+for item in choose:
+    answer += (digit_mapping[item])
+
+print(answer)
